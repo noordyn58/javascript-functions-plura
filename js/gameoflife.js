67a1c5e2488829b5dbf27c223b9cbdfb3a526d75
noordyn58 <noordyn58@gmail.com>
2021-11-02 16:13:18 +0100

    Update gameoflife.js

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -63,30 +63,11 @@ const willBeAlive = (cell, state) => {
   );
 };
 
-const calculateNext = (state) => {
-const calculateNext = (state) => {
-    const { bottomLeft, topRight } = corners(state);
-    let result = [];
-    for (let y = topRight[1] + 1; y >= bottomLeft[1] - 1; y--) {
-      for (let x = bottomLeft[0] - 1; x <= topRight[0] + 1; x++) {
-        result = result.concat(willBeAlive([x, y], state) ? [[x, y]] : []);
-      }
-    }
-    return result;
-};
+const calculateNext = (state) => {};
 
-const iterate = (state, iterations) => {
-  const states = [state];
-  for(let i = 0; i < iterations; i++) {
-      states.push(calculateNext(states[states.length-1]));
-  }
-  return states;
-};
+const iterate = (state, iterations) => {};
 
-const main = (pattern, iterations) => {
-  const results = iterate(startPatterns[pattern], iterations);
-  results.forEach(r => console.log(printCells(r)));
-};
+const main = (pattern, iterations) => {};
 
 const startPatterns = {
     rpentomino: [
